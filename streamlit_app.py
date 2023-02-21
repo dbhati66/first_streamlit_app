5fffecd5753391586c15266a241556f7a8c9e5c8
dbhati66 <deepakbhati66@gmail.com>
2023-02-21 12:07:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 
 import pandas
-my_fruit_list=pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.Datarame(my_fruit_list)
