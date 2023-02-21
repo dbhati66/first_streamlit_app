e3e0c4ebc188dc5eaa128e342aa411e2cb456d5e
dbhati66 <deepakbhati66@gmail.com>
2023-02-21 11:52:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ streamlit.text('Omega 3 and Blueberry Oatmeal')
 
 
 import pandas
-my_fruit_list =pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
