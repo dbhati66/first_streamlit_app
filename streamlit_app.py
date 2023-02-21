becf6adba4d2ddb694a7778feb9ce58f09fecb65
dbhati66 <deepakbhati66@gmail.com>
2023-02-21 11:44:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
-streamlit.title('My Paremts new healthy diner')
+streamlit.title('My Parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
