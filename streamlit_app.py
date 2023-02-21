3e62972df4cef955cc775fb901adb4ad353484fc
dbhati66 <deepakbhati66@gmail.com>
2023-02-21 11:56:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.title('My Parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 
-
+python -m pip install --upgrade pip
 import pandas
 my_fruit_list=pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
