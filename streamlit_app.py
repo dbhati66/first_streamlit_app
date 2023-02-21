ad40f33912091fb2efef3c4bb0c33fed29d99b01
dbhati66 <deepakbhati66@gmail.com>
2023-02-21 13:05:50 +0530

    Update streamlit_app.py
    added new features

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,10 @@ streamlit.text('Omega 3 and Blueberry Oatmeal')
 
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
+# lets put a pick list here
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+#display thr table
 streamlit.dataframe(my_fruit_list)
